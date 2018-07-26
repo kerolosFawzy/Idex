b91744e2cffbaa82dba76dbf5aae477408a3ffae
Kero fawzy <kerofawzy2055@gmail.com>
2018-07-26 11:31:33 +0200

    added package name

---
+++ IDEX/IDEX/Model/Inspection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDEX.Model
+{
+    class Inspection
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ IDEX/IDEX/Model/Scheme.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDEX.Model
+{
+    class Scheme
+    {
+        public int ID { get; set; }
+        public string SchemeName { get; set; }
+    }
+}
