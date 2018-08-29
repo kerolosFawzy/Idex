f421fa58e2a63203cec54cf3e5a15aff539a3178
Kero fawzy <kerofawzy2055@gmail.com>
2018-08-29 13:08:35 +0200

    start coding in instaPage

---
+++ IDEX/IDEX/Views/InstaPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IDEX.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class InstaPage
+	{
+		public InstaPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
